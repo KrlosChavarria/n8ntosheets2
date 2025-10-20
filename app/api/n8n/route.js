92ea7f86fbf525fd0917cb259a92ff3191661f6a
KrlosChavarria <krloschava14@gmail.com>
2025-10-20 10:34:20 -0600

    cambios url

--- app/api/n8n/route.js
+++ app/api/n8n/route.js
@@ -4,7 +4,7 @@ export async function POST(req) {
   try {
     const body = await req.json(); // { spreadsheetId, sheetName }
     // const url = process.env.N8N_WEBHOOK_URL; // debe ser /webhook/... en prod
-    const url = "https://auto.n8npoli.io/webhook-test/da35bcfb-3f79-439e-a368-712400c896b2"; // debe ser /webhook/... en prod
+    const url = "https://auto.n8npoli.io/webhook/da35bcfb-3f79-439e-a368-712400c896b2"; // debe ser /webhook/... en prod
     if (!url) return new Response("Missing N8N_WEBHOOK_URL", { status: 500 });
 
     const r = await fetch(url, {
